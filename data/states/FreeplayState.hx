313e32961db284c501eb7a8a710fb66672a01c7f
Tedyes <tedyes987@gmail.com>
2024-12-21 23:47:50 +0800

    simplecore

---
+++ data/states/FreeplayState.hx
@@ -0,0 +1,39 @@
+import lime.app.Application;
+import funkin.options.OptionsMenu;
+import funkin.menus.credits.CreditsMain;
+import funkin.menus.ModSwitchMenu;
+import funkin.editors.EditorPicker;
+
+function postCreate(){
+	add(versionText = new FunkinText(5, FlxG.height - 2, 0, 'Codename Engine v' + Application.current.meta.get('version') + "\n[ALT] Open Mods menu\n[O] Open Options menu\n[C] Open Credits menu"));
+	versionText.y -= versionText.height;
+}
+
+function update(){
+	FlxG.camera.zoom = lerp(FlxG.camera.zoom,1,0.2);
+	if (FlxG.keys.justPressed.SEVEN) {
+		persistentUpdate = false;
+		persistentDraw = true;
+		openSubState(new EditorPicker());
+	}
+	if (FlxG.keys.justPressed.ALT) {
+		openSubState(new ModSwitchMenu());
+		persistentUpdate = false;
+		persistentDraw = true;
+	}
+	if (FlxG.keys.justPressed.O) {
+		FlxG.switchState(new OptionsMenu());
+	}
+	if (FlxG.keys.justPressed.C) {
+		FlxG.switchState(new CreditsMain());
+	}
+}
+
+function onChangeSelection(_){
+	Conductor.changeBPM(songs[_.value].bpm);
+}
+
+function beatHit(){
+	if (songInstPlaying)
+		FlxG.camera.zoom += 0.05;
+}
--- data/states/MusicBeatTransition.hx
+++ data/states/MusicBeatTransition.hx
@@ -1,8 +1,21 @@
+import openfl.display.Bitmap;
+import openfl.display.BitmapData;
+
 function create() {
 	transitionTween.cancel();
 	remove(blackSpr);
     remove(transitionSprite);
     transitionCamera.scroll.y = 0;
 
-    finish();
+    FlxG.cameras.add(ah = new HudCamera(), false);
+    ah.bgColor = FlxColor.BLACK;
+
+    if (newState == null){ 
+        FlxTween.tween(ah, {alpha: 0.001}, 0.25);
+    }else{
+        ah.alpha = 0.001;
+        FlxTween.tween(ah, {alpha: 1}, 0.25);
+    }
+
+	new FlxTimer().start(0.25, ()-> {finish();});
 }
